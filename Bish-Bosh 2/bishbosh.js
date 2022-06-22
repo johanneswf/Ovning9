7877ae9826a66cd287395d19532beb1660bf8ef7
johanneswf <johanneswf@gmail.com>
2022-06-22 16:07:50 +0200

    Validation

--- Bish-Bosh 2/bishbosh.js
+++ Bish-Bosh 2/bishbosh.js
@@ -8,19 +8,28 @@ const BBrun = function () {
   const length = inputlength.value;
   const bish = inputbish.value;
   const bosh = inputbosh.value;
-  let result = '';
 
+  let result = '';
   for (let i = 1; i <= length; i++) {
-    result += 
-      '<li>' + (   
-          i % bish === 0 && i % bosh === 0 ? 'Bish-Bosh'
-        : i % bish === 0 ? 'Bish'
-        : i % bosh === 0 ? 'Bosh'
-        : i
-      ) + '</li>';
+    result +=
+      '<li>' +
+      ( i % bish === 0 && i % bosh === 0 ? 'Bish-Bosh'
+      : i % bish === 0 ? 'Bish'
+      : i % bosh === 0 ? 'Bosh'
+      : i) + '</li>';
   }
-  result = '<ul class="list-group-item-secondary rounded pb-2 pt-2">' + result + '</ul>';
-  divresult.innerHTML = result;
+
+  divresult.innerHTML = result !== '' 
+    ? '<ul class="list-group-item-success rounded pb-2 pt-2">' + result + '</ul>'
+    : '';
 }
+
+const nonegs = function () {
+  if (this.value < 0) this.value = 0;
+}
+
+
+document.querySelectorAll('input')
+  .forEach(input => input.addEventListener('input', nonegs, false))
 
 btn.addEventListener('click', BBrun);
