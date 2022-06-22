3c9c6b2b5dcffa185dec1db2ea686d8dff20a39c
johanneswf <johanneswf@gmail.com>
2022-06-22 14:03:13 +0200

    Cleanup

--- Bish-Bosh 2/bishbosh.js
+++ Bish-Bosh 2/bishbosh.js
@@ -1,23 +1,34 @@
-document
-  .querySelector("#btnBB")
-  .addEventListener("click", BishBosh);
+const id = document.getElementById("input");
 
-function BishBosh() {
-  const id = document.getElementById("input");
+const BBrun = function () {
   const length = id.elements[0].value;
   const bish = id.elements[1].value;
   const bosh = id.elements[2].value;
   let result = "";
 
   for (let i = 1; i <= length; i++) {
-    result += EvalBB(i, bish, bosh) + "<br>";
+    result +=
+      (   i % bish === 0 && i % bosh === 0 ? 'Bish-Bosh'
+        : i % bish === 0 ? 'Bish'
+        : i % bosh === 0 ? 'Bosh'
+        : i
+      ) + "<br>"
     document.getElementById("result").innerHTML = result;
   }
 }
 
-function EvalBB(i, bish, bosh) {
+/* const BBeval = function (i, bish, bosh) {
+  //  if (i % bish === 0 && i % bosh === 0) return 'Bish-Bosh';
+  //  else if (i % bish === 0) return 'Bish';
+  //  else if (i % bosh === 0) return 'Bosh';
+  //  else return i;
+
   return (i % bish === 0 && i % bosh === 0) ? 'Bish-Bosh'
-       : (i % bish === 0) ? 'Bish'
-       : (i % bosh === 0) ? 'Bosh'
-       : i;
-}
+    : (i % bish === 0) ? 'Bish'
+      : (i % bosh === 0) ? 'Bosh'
+        : i;
+} */
+
+document
+  .querySelector("#btnBB")
+  .addEventListener("click", BBrun);
