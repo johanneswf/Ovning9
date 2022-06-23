66f613bfe563850f4dd77543954478a6c2e48e18
johanneswf <johanneswf@gmail.com>
2022-06-23 14:35:23 +0200

    Push

---
+++ ToDo/todo.js
@@ -0,0 +1,33 @@
+const newitem = document.querySelector('#newitem');
+const btnadd = document.querySelector('#additem');
+const itemlist = document.querySelector('#itemlist');
+
+const itemAdd = function () {
+  const btn = document.createElement('button');
+  btn.id = 'removeitem';
+  btn.className = 'btn btn-sm btn-danger float-end';
+  btn.innerText = 'Remove';
+
+  const li = document.createElement('li');
+  li.className = 'list-group-item';
+  li.innerText = newitem.value;
+  li.id = 'unstrike';
+  li.append(btn);
+
+  itemlist.appendChild(li);
+}
+
+const itemRemove = function () {
+  console.log(this);
+}
+
+const itemToggle = function (e) {
+  if (e.id === 'unstrike') e.id = 'strike';
+  else if (e.id === 'strike') e.id = 'unstrike';
+}
+
+
+
+btnadd.addEventListener('click', itemAdd);
+
+itemlist.addEventListener('click', e => itemToggle(e.target));
