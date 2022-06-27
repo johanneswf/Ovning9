ab2b5b184880b67914f9ec2a6f56ff21590ee4ef
johanneswf <johanneswf@gmail.com>
2022-06-27 12:19:33 +0200

    Functionality Resets input after adding item. Toggling the strike-through on items now always strike-throughs first.

--- ToDo/todo.js
+++ ToDo/todo.js
@@ -5,21 +5,26 @@ const btnmark = document.querySelector('#markall');
 
 
 btnadd.addEventListener('click', () => {
-  const btn = document.createElement('button');
-  btn.id = 'removeitem';
-  btn.className = 'btn btn-sm btn-danger float-end';
-  btn.innerText = 'Remove';
+  if (newitem.value !== '') {
+    const btn = document.createElement('button');
+    btn.id = 'removeitem';
+    btn.className = 'btn btn-sm btn-danger float-end';
+    btn.innerText = 'Remove';
 
-  const li = document.createElement('li');
-  li.className = 'list-group-item';
-  li.innerText = newitem.value;
-  li.append(btn);
+    const li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.innerText = newitem.value;
+    li.append(btn);
 
-  itemlist.appendChild(li);
-  btn.addEventListener('click', e => e.target.parentNode.remove());
+    itemlist.appendChild(li);
+    btn.addEventListener('click', e => e.target.parentNode.remove());
+
+    newitem.value = '';
+  }
 });
 
 itemlist.addEventListener('click', (e) => {
+  e.target.className = 'strike';
   e.target.classList.toggle('strike');
 });
 
